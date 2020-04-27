87c0a2e37dcf64c913f397e37aa6ba3d7494de2d
Mehmet Teker <teker.mehmed@gmail.com>
2020-04-27 12:38:16 +0300

    RabbitMQ Exchange çeşitlerinde Fanout örneği

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -10,39 +10,37 @@ namespace RabbitMQ.Consumer
     {
         private static void Main(string[] args)
         {
-            if(args == null) {
-                Console.WriteLine("Args null");
-            }
-            else {
-                args = new string[1];
-                args[0] = "100";
-            }
 
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqp://ugtyfnnw:UNR12-I4-zKp_dU4mjcmTmuK3PHd3Hz-@rattlesnake.rmq.cloudamqp.com/ugtyfnnw");
+
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare("task_queue", true, false, false, null);
+                { 
+                    channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                    var queueName = channel.QueueDeclare().QueueName; 
+                    channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+                    channel.BasicQos(prefetchSize: 0, prefetchCount: 1, false);
+                    
+                    Console.WriteLine("Logları bekliyorum....");
                     var consumer = new EventingBasicConsumer(channel);
-                    channel.BasicQos(0, 1, false);
-                    Console.WriteLine("mesajları bekliyorum....");
+                     
+                    channel.BasicConsume(queueName, false, consumer);
 
                     consumer.Received += (model, ea) =>
-                     {
-                         var message =  Encoding.UTF8.GetString(ea.Body.ToArray());
-                         Console.WriteLine("Mesaj Alındı:" + message);
+                    {
+                        var message = Encoding.UTF8.GetString(ea.Body.ToArray());
+                        Console.WriteLine("Log Alındı:" + message);
 
-                         int time = int.Parse(GetMessage(args));
-                         Thread.Sleep(time);
-                         Console.WriteLine("Mesaj işlendi...");
+                        int time = int.Parse(GetMessage(args));
+                        Thread.Sleep(time);
+                        Console.WriteLine("Loglama Tamamlandı...");
 
-                         channel.BasicAck(ea.DeliveryTag, false);
-                     };
+                        channel.BasicAck(ea.DeliveryTag, multiple: false);
+                    };
 
-                    channel.BasicConsume("task_queue", false, consumer);
-
+                    
                 }
 
                 Console.WriteLine("Çıkış yapmak için tıklayınız..");
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -8,39 +8,29 @@ namespace RabbitMQ.Publisher
     {
         private static void Main(string[] args)
         {
-            if (args == null) {
-                Console.WriteLine("Args null");
-            }
-            else {
-                args = new string[1];
-                args[0] = "Mehmet";
-            }
-
-
             var factory = new ConnectionFactory();
             factory.Uri = new Uri("amqp://ugtyfnnw:UNR12-I4-zKp_dU4mjcmTmuK3PHd3Hz-@rattlesnake.rmq.cloudamqp.com/ugtyfnnw");
+
             using (var connection = factory.CreateConnection())
             {
                 using (var channel = connection.CreateModel())
-                {
-                    channel.QueueDeclare("task_queue",true, false, false, null);
+                { 
+                    channel.ExchangeDeclare("logs", durable: true, type: ExchangeType.Fanout);
+                     
                     string message = GetMessage(args);
                     for (int i = 1; i < 11; i++)
-                    {
-                        var bodyMessage = Encoding.UTF8.GetBytes($"{message}-{i}");
-
+                    { 
+                        var bodyByte = Encoding.UTF8.GetBytes($"{message}-{i}");
                         var properties = channel.CreateBasicProperties();
                         properties.Persistent = true;
-                        channel.BasicPublish("", "task_queue",properties, bodyMessage);
-                        Console.WriteLine("Mesajınız Gönderilmiştir."+ $"{message}-{i}");
+                        channel.BasicPublish("logs", routingKey: "", properties, body: bodyByte);
+                        Console.WriteLine($"Mesajınız gönderilmiştir:{message}-{i}");
                     }
                 }
 
                 Console.WriteLine("Çıkış yapmak için tıklayınız..");
                 Console.ReadLine();
             }
-
-
         }
 
         private static string GetMessage(string[] args)
